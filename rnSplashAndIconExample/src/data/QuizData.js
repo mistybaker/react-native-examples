c1dcee20b95cc6c412a62b34f54703f4a94a79ab
mistybaker <farkask4826@gmail.com>
2021-12-07 11:48:29 +0100

    update

---
+++ rnSplashAndIconExample/src/data/QuizData.js
@@ -0,0 +1,53 @@
+export default data = [
+    {
+        question: "Melyik kifejezés nem kapcsolható közvetlenül a relációs adatbázisokhoz?",
+        options: ["Encapsulation","Primary Key","Redundancy"],
+        correct_option: "Encapsulation"
+    },
+    {
+        question: "Melyik frekvenciát használják a Bluetooth eszközök?",
+        options: ["24 GHz","900 MHz","2,4 GHz"],
+        correct_option: "2,4 GHz"
+    },
+    {
+        question: "A felsoroltak közül melyik alkalmas algoritmusok leírásához?",
+        options: ["Stuktogram","Szelekció","Venn-diagram"],
+        correct_option: "Stuktogram"
+    },
+    {
+        question: "Az alábbiak közül mely funkciót lehet beállítani a BIOS-ban?",
+        options: ["A merevlemezek partícioit","Virtuális memória méretét","Alaplapi eszközök kikapcsolását"],
+        correct_option: "Alaplapi eszközök kikapcsolását"
+    },
+    {
+        question: "Az alábbiak közül mely egységek csatlakoztatásához használhatunk SATA csatlakozót?",
+        options: ["Monitor","Optikai meghajtó","Egér"],
+        correct_option: "Optikai meghajtó"
+    },
+    {
+        question: "Az alábbiak közül melyik lemezkarbantartó program?",
+        options: ["POST","CHKDSK","BIOS"],
+        correct_option: "CHKDSK"
+    },
+    {
+        question: "Minek a rövidítése az USB?",
+        options: ["Universal Serial Box","Universal Serial Bus","Universal Sealed Bus"],
+        correct_option: "Universal Serial Bus"
+    },
+    {
+        question: "Minek a rövidítése a Wi-Fi?",
+        options: ["Wireless Fidelity","Width Free","Wireless Finder"],
+        correct_option: "Wireless Fidelity"
+    },
+    {
+        question: "Az alábbi felsorolásban melyik nem tekinthető erősen típusos nyelvnek?",
+        options: ["C++","Java","JavaScript"],
+        correct_option: "JavaScript"
+    },
+    {
+        question: "Mi volt az első objektum-orientált programozási nyelv?",
+        options: ["Simula 67","C++","Visual Basic"],
+        correct_option: "Simula 67"
+    }
+    
+]
